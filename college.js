805465ddbd5b04d7f5ea753b8f8ac5cded6efe08
Foysal Bin Nour <86665642+foysalBN@users.noreply.github.com>
2024-06-13 18:50:13 +0600

    Update college.js

--- college.js
+++ college.js
@@ -12,3 +12,15 @@ let [pf_min, pf_max] = [48, 55]
 
 document.querySelectorAll('table[width="100%"] td input:nth-child(3)').forEach(input => input.value = Math.floor(Math.random() * (pf_max - pf_min +1)) + pf_min);
 //============================ END ============================
+
+
+// =============== Random Mark Generator START ===============
+let toppers=[]
+let [pf_min, pf_max] = [0, 0]
+
+document.querySelectorAll('table[width="100%"] tr tr').forEach(tr=>{
+    return toppers.includes(parseInt(tr.querySelector('td[align="left"]').innerText))
+        ?(tr.querySelector('input[type="text"]').value=pf_max, tr.querySelectorAll('*').forEach(td=> td.style.backgroundColor = '#00fd0838'))  // Topper
+        :tr.querySelector('input[type="text"]').value= Math.floor(Math.random() * (pf_max - pf_min )) + pf_min //Random Student
+})
+//============================ END ============================
